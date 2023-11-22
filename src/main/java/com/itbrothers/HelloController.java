32a3a8ffb0ea88b4de870f3410202f7963cd8d42
RonR <RRuffin@nciinc.com>
2023-11-22 10:40:41 -0500

    Added crucial notes for how to access this application once deployed.

--- src/main/java/com/itbrothers/HelloController.java
+++ src/main/java/com/itbrothers/HelloController.java
@@ -1,7 +1,9 @@
 package com.itbrothers;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,10 +17,23 @@ public class HelloController {
 		System.out.println("HelloController ...");
 	}
 	
-	@RequestMapping({"/test.spring","testJboss7"})
-	public String testController(){
+	@RequestMapping({ "/test.spring", "testJboss7" })
+	public String testController() {
+		// From URL: server-name:8080/SPRINGBOOTJBOSS/test.spring
+		// Response is "Hello"
+		// From URL: server-name:8080/SPRINGBOOTJBOSS/testJboss7
+		// Response is "Hello"
 		System.out.println("testController ...");
 		return "Hello";
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		// From URL: server-name:8080/SPRINGBOOTJBOSS/hello
+		// Response is "Hello World!"
+		// From URL: server-name:8080/SPRINGBOOTJBOSS/hello?name=Anna
+		// Response is "Hello Anna!"
+		return String.format("Hello %s!", name);
 	}
 	
 }
